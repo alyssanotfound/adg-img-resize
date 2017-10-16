135136e275a97316243ae9ebf1c1fe0f3472bd3d
Alyssa Davis <davia317@gmail.com>
2017-10-16 16:41:21 -0400

    width and height from sys.argv

--- resize-img.py
+++ resize-img.py
@@ -1,6 +1,7 @@
 # format images for Alyssa Davis Gallery 
 # run script in a directory that contains a directory called "original_images"
 # with all original images inside. these won't be deleted
+# run python resize-img.py width height
 
 from __future__ import print_function
 from __future__ import division
@@ -12,6 +13,9 @@ print("~ starting script ~")
 directory = "original_images"
 thumbdir = "thumbnails"
 carouseldir = "carousel_images"
+desired_w = int(sys.argv[1])
+desired_h = int(sys.argv[2])
+print("desired width: ", desired_w, "desired height: ", desired_h)
 
 if not os.path.exists(thumbdir):
     os.makedirs(thumbdir)
@@ -28,41 +32,37 @@ for infile in os.listdir(directory):
             width, height = im.size
             # add case to only resize if aspect ratio matches
             print("w/h: ", width/height)
-            print("aspect ratio: ", round(235/157, 1))
-            if (round(width/height,1)) == round(235/157, 1):
+            print("aspect ratio: ", round(desired_w/desired_h, 1))
+            if (round(width/height,1)) == round(desired_w/desired_h, 1):
                 print("aspect ratio matches")
-                new_width = 235
-                new_height = 157
+                new_width = desired_w
+                new_height = desired_h
                 final_img = im
                 final_img.thumbnail((new_width, new_height), Image.ANTIALIAS)
             else:   
                 if width > height:
                     print("width greater than height")
-                    new_width = 235
+                    new_width = desired_w
                     new_height = int(round(new_width/(width/height)))
                     print("new h: ", new_height) 
-                    transparent_h = (157 - new_height)/2
+                    transparent_h = (desired_h - new_height)/2
                     transparent_h = int(round(transparent_h))
-                    transparent_w = 235
+                    transparent_w = desired_w
                     print("new width w transparency", new_width)
                     print('new height with transparency:', transparent_h+new_height+transparent_h)
-                    if (transparent_h+new_height+transparent_h) != 157:
+                    if (transparent_h+new_height+transparent_h) != desired_h:
                         print('height doesnt match')
                 elif width <= height:
                     print("height greater than width")
-                    new_height = 157
+                    new_height = desired_h
                     new_width = int(round((width/height)*new_height))
-                    transparent_h = 157
-                    transparent_w = (235 - new_width)/2
+                    transparent_h = desired_h
+                    transparent_w = (desired_w - new_width)/2
                     transparent_w = int(round(transparent_w))
                     print("new height w transparency", new_height)
                     print('new width w transparency:', transparent_w+new_width+transparent_w)
-                    if (transparent_w+new_width+transparent_w) != 235:
+                    if (transparent_w+new_width+transparent_w) != desired_w:
                         print('height doesnt match')
-                # print(width)
-                # print(height)
-                # print(transparent_w)
-                # print("transparent h: ", transparent_h)
                 # save resized image then overwrite with transparency
                 size = (new_width, new_height)
                 print("size of resized img: ", size)
@@ -79,7 +79,7 @@ for infile in os.listdir(directory):
                 images = map(Image.open, [t_box_path, outfile, t_box_path])
                 print("images: ", images)
 
-                # os.remove(os.path.splitext(infile)[0] + "-t-box.png")
+                os.remove(t_box_path)
                 widths, heights = zip(*(i.size for i in images))
 
                 if width > height:
@@ -100,7 +100,6 @@ for infile in os.listdir(directory):
                       final_img.paste(im, (x_offset,0))
                       x_offset += im.size[0]
 
-            
             print(final_img.size)
             final_img.save(outfile, 'PNG')
             
